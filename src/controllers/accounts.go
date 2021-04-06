aa66c910076bf648a92a33492bed9ae305c1644e
Leandro Souza <leandrossouza1@gmail.com>
2021-04-06 10:08:13 -0300

    refactor: Move accounts request handler to controllers
    Add functions to handle each request

---
+++ src/controllers/accounts.go
@@ -0,0 +1,18 @@
+package controllers
+
+import "net/http"
+
+// GetAccounts return a list of accounts
+func GetAccounts(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// GetAccountBalance return a account balance
+func GetAccountBalance(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// CreateAccount insert a new account on database
+func CreateAccount(w http.ResponseWriter, r *http.Request) {
+
+}
--- src/routes/accounts.go
+++ src/routes/accounts.go
@@ -1,32 +1,27 @@
 package routes
 
 import (
+	"challenge-golang-stone/src/controllers"
 	"net/http"
 )
 
 var accountRoutes = []Route{
 	{
-		URI:    "/accounts",
-		Method: http.MethodGet,
-		Function: func(w http.ResponseWriter, r *http.Request) {
-
-		},
+		URI:        "/accounts",
+		Method:     http.MethodGet,
+		Function:   controllers.GetAccounts,
 		AuthNeeded: false,
 	},
 	{
-		URI:    "/accounts/{account_id}/balance",
-		Method: http.MethodGet,
-		Function: func(w http.ResponseWriter, r *http.Request) {
-
-		},
+		URI:        "/accounts/{account_id}/balance",
+		Method:     http.MethodGet,
+		Function:   controllers.GetAccountBalance,
 		AuthNeeded: false,
 	},
 	{
-		URI:    "/accounts",
-		Method: http.MethodPost,
-		Function: func(w http.ResponseWriter, r *http.Request) {
-
-		},
+		URI:        "/accounts",
+		Method:     http.MethodPost,
+		Function:   controllers.CreateAccount,
 		AuthNeeded: false,
 	},
 }
