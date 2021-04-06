19a18544ee5706001fcddbc620c9e3c1aa087719
Leandro Souza <leandrossouza1@gmail.com>
2021-04-06 14:00:12 -0300

    feat: Add validate functions to Account model
    functions to format and validate fields before save in database

--- src/models/account.go
+++ src/models/account.go
@@ -1,6 +1,13 @@
 package models
 
-import "time"
+import (
+	"challenge-golang-stone/src/security"
+	"challenge-golang-stone/src/validators"
+	"errors"
+	"regexp"
+	"strings"
+	"time"
+)
 
 // Account model for accounts
 type Account struct {
@@ -10,4 +17,58 @@ type Account struct {
 	Secret    string    `json:"secret,omitempty"`
 	Balance   int       `json:"balance,omitempty"`
 	CreatedAt time.Time `json:"created_at,omitempty"`
+}
+
+// Prepare format and validate account fields
+func (account *Account) Prepare() error {
+	if err := account.format(); err != nil {
+		return err
+	}
+	if err := account.validate(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (account *Account) validate() error {
+	if account.Name == "" {
+		return errors.New("Name is a required field")
+	}
+	if account.Cpf == "" {
+		return errors.New("CPF is a required field")
+	}
+	if !validators.ValidateCPF(account.Cpf) {
+		return errors.New("CPF invalid")
+	}
+	if account.Secret == "" {
+		return errors.New("Secret is a required field")
+	}
+	if account.Balance != 0 {
+		return errors.New("Balance must init with 0")
+	}
+
+	return nil
+}
+
+func (account *Account) format() error {
+	account.Name = strings.TrimSpace(account.Name)
+	account.Cpf = strings.TrimSpace(account.Cpf)
+
+	reg, err := regexp.Compile("\\D")
+	if err != nil {
+		return err
+	}
+
+	account.Cpf = reg.ReplaceAllString(account.Cpf, "")
+
+	hashedSecret, err := security.Hash(account.Secret)
+	if err != nil {
+		return err
+	}
+
+	account.Secret = string(hashedSecret)
+	account.Balance = 0
+
+	return nil
 }
