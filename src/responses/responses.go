764dce11937fc726530163771241b0ef2108649d
Leandro Souza <leandrossouza1@gmail.com>
2021-04-06 11:11:15 -0300

    feat: Create a package to handle responses

---
+++ src/responses/responses.go
@@ -0,0 +1,29 @@
+package responses
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+// JSON return a JSON response
+func JSON(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+
+	if data != nil {
+		if err := json.NewEncoder(w).Encode(data); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+}
+
+// Error returns a error as JSON
+func Error(w http.ResponseWriter, statusCode int, err error) {
+	JSON(w, statusCode, struct {
+		Error string `json:"error"`
+	}{
+		Error: err.Error(),
+	})
+}
