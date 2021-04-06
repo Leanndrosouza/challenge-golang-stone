ded5b7c524bb2b2fb5d98b16c4ff52ac110618ee
Leandro Souza <leandrossouza1@gmail.com>
2021-04-06 14:18:15 -0300

    feat: Add function CreateAccount to accounts controller

--- src/controllers/accounts.go
+++ src/controllers/accounts.go
@@ -1,6 +1,14 @@
 package controllers
 
-import "net/http"
+import (
+	"challenge-golang-stone/src/database"
+	"challenge-golang-stone/src/models"
+	"challenge-golang-stone/src/repositories"
+	"challenge-golang-stone/src/responses"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
 
 // GetAccounts return a list of accounts
 func GetAccounts(w http.ResponseWriter, r *http.Request) {
@@ -14,5 +22,39 @@ func GetAccountBalance(w http.ResponseWriter, r *http.Request) {
 
 // CreateAccount insert a new account on database
 func CreateAccount(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
 
+	if err != nil {
+		responses.Error(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	var account models.Account
+
+	if err = json.Unmarshal(body, &account); err != nil {
+		responses.Error(w, http.StatusBadRequest, err)
+		return
+	}
+
+	if err := account.Prepare(); err != nil {
+		responses.Error(w, http.StatusBadRequest, err)
+		return
+	}
+
+	db, err := database.Connect()
+	if err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer db.Close()
+
+	repository := repositories.NewAccountRepository(db)
+	account.ID, err = repository.Create(account)
+
+	if err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	responses.JSON(w, http.StatusCreated, account)
 }
