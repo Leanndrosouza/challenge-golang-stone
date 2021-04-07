569be2f1310ad9250860c290d32ddb7bf3288297
Leandro Souza <leandrossouza1@gmail.com>
2021-04-07 00:40:01 -0300

    feat: Add sql to create transfers table

--- sql/sql.sql
+++ sql/sql.sql
@@ -11,4 +11,16 @@ CREATE TABLE accounts(
     secret varchar(100) not null,
     balance int not null,
     created_at timestamp default current_timestamp()
+);
+
+DROP TABLE IF EXISTS transfers;
+
+CREATE TABLE transfers(
+    id int auto_increment primary key,
+    account_origin_id int not null REFERENCES accounts(id),
+    account_destination_id int not null REFERENCES accounts(id),
+    amount int not null,
+    created_at timestamp default current_timestamp(),
+    FOREIGN KEY (account_origin_id) REFERENCES accounts(id) ON DELETE CASCADE,
+    FOREIGN KEY (account_destination_id) REFERENCES accounts(id) ON DELETE CASCADE
 ) ENGINE=INNODB
