e417e74c12df1f39a154a13e0f8eeedbece8f34a
Leandro Souza <leandrossouza1@gmail.com>
2021-04-07 14:22:15 -0300

    feat: Add function to transfer controller to transfer amount

--- src/controllers/transfers.go
+++ src/controllers/transfers.go
@@ -3,8 +3,11 @@ package controllers
 import (
 	"challenge-golang-stone/src/auth"
 	"challenge-golang-stone/src/database"
+	"challenge-golang-stone/src/models"
 	"challenge-golang-stone/src/repositories"
 	"challenge-golang-stone/src/responses"
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -18,6 +21,10 @@ func GetTransfers(w http.ResponseWriter, r *http.Request) {
 	defer db.Close()
 
 	originID, err := auth.ExtractUserID(r)
+	if err != nil {
+		responses.Error(w, http.StatusUnauthorized, err)
+		return
+	}
 
 	repository := repositories.NewTransferRepository(db)
 	transfers, err := repository.GetByOriginID(originID)
@@ -32,5 +39,51 @@ func GetTransfers(w http.ResponseWriter, r *http.Request) {
 
 // Transfer make a tranfer between two accounts
 func Transfer(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
 
+	if err != nil {
+		responses.Error(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	var transfer models.Transfer
+
+	if err = json.Unmarshal(body, &transfer); err != nil {
+		responses.Error(w, http.StatusBadRequest, err)
+		return
+	}
+
+	transfer.AccountOriginID, err = auth.ExtractUserID(r)
+	if err != nil {
+		responses.Error(w, http.StatusUnauthorized, err)
+		return
+	}
+
+	if err := transfer.Prepare(); err != nil {
+		responses.Error(w, http.StatusBadRequest, err)
+		return
+	}
+
+	db, err := database.Connect()
+	if err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer db.Close()
+
+	repository := repositories.NewTransferRepository(db)
+
+	err = repository.CanTransfer(transfer)
+
+	if err != nil {
+		responses.Error(w, http.StatusUnauthorized, err)
+		return
+	}
+
+	if err = repository.Transfer(transfer); err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	responses.JSON(w, http.StatusOK, nil)
 }
