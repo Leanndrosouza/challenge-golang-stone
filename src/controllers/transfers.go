974deed556fe043d0d937a04b7774013d1fa3dc3
Leandro Souza <leandrossouza1@gmail.com>
2021-04-07 10:29:26 -0300

    feat: Add a function to get transfers of auth user

--- src/controllers/transfers.go
+++ src/controllers/transfers.go
@@ -1,10 +1,33 @@
 package controllers
 
-import "net/http"
+import (
+	"challenge-golang-stone/src/auth"
+	"challenge-golang-stone/src/database"
+	"challenge-golang-stone/src/repositories"
+	"challenge-golang-stone/src/responses"
+	"net/http"
+)
 
 // GetTransfers returns all transfers of a authenticated user
 func GetTransfers(w http.ResponseWriter, r *http.Request) {
+	db, err := database.Connect()
+	if err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer db.Close()
 
+	originID, err := auth.ExtractUserID(r)
+
+	repository := repositories.NewTransferRepository(db)
+	transfers, err := repository.GetByOriginID(originID)
+
+	if err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	responses.JSON(w, http.StatusOK, transfers)
 }
 
 // Transfer make a tranfer between two accounts
