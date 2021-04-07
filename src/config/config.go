93c004060b4f709e332d459eddf850fa57b5dff6
Leandro Souza <leandrossouza1@gmail.com>
2021-04-07 15:06:18 -0300

    feat: Add config to run api on docker

--- src/config/config.go
+++ src/config/config.go
@@ -33,9 +33,10 @@ func Load() {
 		Port = 9000
 	}
 
-	ConnectionString = fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
+	ConnectionString = fmt.Sprintf("%s:%s@(%s)/%s?charset=utf8&parseTime=True&loc=Local",
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
 		os.Getenv("DB_NAME"),
 	)
 
