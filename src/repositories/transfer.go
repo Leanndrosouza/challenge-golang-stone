3d1a2815ed5455d6e8b5079ef206abb676ae14fc
Leandro Souza <leandrossouza1@gmail.com>
2021-04-07 14:21:31 -0300

    feat: Add functions to transfer repository to handle transfers operation

--- src/repositories/transfer.go
+++ src/repositories/transfer.go
@@ -3,6 +3,7 @@ package repositories
 import (
 	"challenge-golang-stone/src/models"
 	"database/sql"
+	"errors"
 )
 
 // Transfers is a repository for transfers
@@ -48,24 +49,117 @@ func (repository Transfers) GetByOriginID(originID uint64) ([]models.Transfer, e
 }
 
 // Transfer make a transfer between two accounts
-func (repository Transfers) Transfer(transfer models.Transfer) (uint64, error) {
-	statement, err := repository.db.Prepare(
-		"insert into transfers (account_origin_id, account_destination_id, amount) values (?,?,?)",
-	)
+func (repository Transfers) Transfer(transfer models.Transfer) error {
+	tx, err := repository.db.Begin()
 	if err != nil {
-		return 0, err
-	}
-	defer statement.Close()
-
-	result, err := statement.Exec(transfer.AccountOriginID, transfer.AccountDestinationID, transfer.Amount)
-	if err != nil {
-		return 0, err
+		return err
 	}
 
-	lastIDInserted, err := result.LastInsertId()
+	newOriginBalance, newDestinationBalance, err := repository.amountAfterTransfer(transfer)
 	if err != nil {
-		return 0, err
+		return err
 	}
 
-	return uint64(lastIDInserted), nil
+	{
+		stmt, err := tx.Prepare("insert into transfers (account_origin_id, account_destination_id, amount) values (?,?,?)")
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+		defer stmt.Close()
+
+		if _, err := stmt.Exec(transfer.AccountOriginID, transfer.AccountDestinationID, transfer.Amount); err != nil {
+			tx.Rollback()
+			return err
+		}
+	}
+
+	{
+		stmt, err := tx.Prepare("update accounts set balance = ? where id = ?")
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+		defer stmt.Close()
+
+		if _, err := stmt.Exec(newOriginBalance, transfer.AccountOriginID); err != nil {
+			tx.Rollback()
+			return err
+		}
+	}
+
+	{
+		stmt, err := tx.Prepare("update accounts set balance = ? where id = ?")
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+		defer stmt.Close()
+
+		if _, err := stmt.Exec(newDestinationBalance, transfer.AccountDestinationID); err != nil {
+			tx.Rollback()
+			return err
+		}
+	}
+
+	return tx.Commit()
+}
+
+// CanTransfer returns a error if transfer can't be done
+func (repository Transfers) CanTransfer(transfer models.Transfer) error {
+	if transfer.AccountDestinationID == transfer.AccountOriginID {
+		return errors.New("Invalid destination account")
+	}
+
+	if transfer.Amount <= 0 {
+		return errors.New("Invalid transfer amount")
+	}
+
+	accountRepository := NewAccountRepository(repository.db)
+
+	originAccount, err := accountRepository.SearchByID(transfer.AccountOriginID)
+	if err != nil {
+		if err.Error() == "Account not found" {
+			return errors.New("Origin Account not found")
+		}
+		return err
+	}
+
+	_, err = accountRepository.SearchByID(transfer.AccountDestinationID)
+	if err != nil {
+		if err.Error() == "Account not found" {
+			return errors.New("Destination Account not found")
+		}
+		return err
+	}
+
+	if originAccount.Balance < transfer.Amount {
+		return errors.New("No balance to make this transfer")
+	}
+
+	return nil
+}
+
+func (repository Transfers) amountAfterTransfer(transfer models.Transfer) (int, int, error) {
+	accountRepository := NewAccountRepository(repository.db)
+
+	originAccount, err := accountRepository.SearchByID(transfer.AccountOriginID)
+	if err != nil {
+		if err.Error() == "Account not found" {
+			return 0, 0, errors.New("Origin Account not found")
+		}
+		return 0, 0, err
+	}
+
+	detinationAccount, err := accountRepository.SearchByID(transfer.AccountDestinationID)
+	if err != nil {
+		if err.Error() == "Account not found" {
+			return 0, 0, errors.New("Destination Account not found")
+		}
+		return 0, 0, err
+	}
+
+	return originAccount.Balance - transfer.Amount,
+		detinationAccount.Balance + transfer.Amount,
+		nil
 }
